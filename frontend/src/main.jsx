1fec27626cb01285b3834fc2e4bddc80cf6849c2
marlon agusto <mmoralesc54@miumg.edu.gt>
2025-09-02 01:10:15 -0600

    Restore full React application with complete imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function App() {
-  return <div><h1>Hello CASIRA</h1></div>
-}
-
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
